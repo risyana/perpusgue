1ef2f16b059300d44e81aef1a3d2d785c915d1ee
Eka Risyana <risyana@gmail.com>
2017-05-11 18:29:36 +0700

    add validation using html 5 standard validation

--- index.php
+++ index.php
@@ -34,41 +34,44 @@
 
 		?>
 
-		<h2>Perpus Gue</h2>
+		<h1><a href="/PerpusGue">Perpus Gue</a></h1>
 
-		<form style="<?echo $formVisible;?>" action="crud.php?mode=<?echo $mode;?>" method="POST">
-			<input name='id' type="hidden" value="<?echo $id ?>" />
-			<input name='title' type="text" placeholder="Title" value="<?echo $value[0]; ?>" <?echo $disabled;?>  /> 
-			<input name='author' type="text" placeholder="Author" value="<?echo $value[1]; ?>" <?echo $disabled;?>  /> 
-			<input name='year' type="text" placeholder="Year" value="<?echo $value[2]; ?>" <?echo $disabled;?>  /> 
-			<input type="submit" value=<?echo $submit_btn; ?> />
-		</form>
+		<div>
+			<form style="<?echo $formVisible;?>" action="crud.php?mode=<?echo $mode;?>" method="POST">
+				<input name='id' type="hidden" value="<?echo $id ?>" />
+				<input name='title' type="text" placeholder="Title" value="<?echo $value[0]; ?>" <?echo $disabled;?>  autofocus autocomplete="off" required/> 
+				<input name='author' type="text" placeholder="Author" value="<?echo $value[1]; ?>" <?echo $disabled;?>  autocomplete="off" required/> 
+				<input name='year' type="number" min=1900 max=3000 placeholder="Year" value="<?echo $value[2]; ?>" <?echo $disabled;?>  autocomplete="off" required/> 
+				<input type="submit" value=<?echo $submit_btn; ?> />
+				<a href="/PerpusGue"><input type="button" value="Cancel" /></a>
+			</form>
 
-		<a id="add" class="btn" href="index.php?mode=add" title="Add Book"><img src='img/b_insrow.png'  /> Add </a>
+			<a id="add" class="btn" href="index.php?mode=add" title="Add Book"><img src='img/b_insrow.png'  /> Add </a>
 
 
-		<div class='table-row'>
-			<span class='table_med' id='head'>Title</span>
-			<span class='table_med' id='head'>Author</span> 
-			<span class='table_med' id='head'>Released </span> 
-			<span class='table_small' id='head'>	
-			</span> 
+			<div class='table-row'>
+				<span class='table_med' id='head'>Title</span>
+				<span class='table_med' id='head'>Author</span> 
+				<span class='table_med' id='head'>Released </span> 
+				<span class='table_small' id='head'>	
+				</span> 
+			</div>
+			<?
+			// Display Data
+			$q = "select t.c_id, t.c_title, t.c_author, t.c_release_year from t_m_books t";
+			$result = mysql_query($q);
+			while($row=mysql_fetch_row($result)){
+				echo "<div class='table-row'>";
+					echo "<span class='table_med'><a href='http://google.com?#q=$row[1] $row[2]' target='_blank' >$row[1]</a></span>";
+					echo "<span class='table_med'>".$row[2]."</span> ";
+					echo "<span class='table_med'>".$row[3]."</span> ";
+					echo "<span class='table_small'>";
+						echo "<a class='btn' href='index.php?mode=update&id=$row[0]' title='Update Book' ><img src='img/b_edit.png' /></a> ";
+						echo "<a class='btn' href='index.php?mode=delete&id=$row[0]' title='Delete Book' ><img src='img/b_drop.png'  /></a> ";
+					echo "</span> ";
+				echo "</div>";
+			}
+			?>
 		</div>
-		<?
-		// Display Data
-		$q = "select t.c_id, t.c_title, t.c_author, t.c_release_year from t_m_books t";
-		$result = mysql_query($q);
-		while($row=mysql_fetch_row($result)){
-			echo "<div class='table-row'>";
-				echo "<span class='table_med'><a href='http://google.com?#q=$row[1] $row[2]' target='_blank' >$row[1]</a></span>";
-				echo "<span class='table_med'>".$row[2]."</span> ";
-				echo "<span class='table_med'>".$row[3]."</span> ";
-				echo "<span class='table_small'>";
-					echo "<a class='btn' href='index.php?mode=update&id=$row[0]' title='Update Book' ><img src='img/b_edit.png' /></a> ";
-					echo "<a class='btn' href='index.php?mode=delete&id=$row[0]' title='Delete Book' ><img src='img/b_drop.png'  /></a> ";
-				echo "</span> ";
-			echo "</div>";
-		}
-		?>
 	</body>
 </html>
